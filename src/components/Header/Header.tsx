03b2bb8d7b7fa0940f1c77ed44122cced2d75ce0
PedroUchoa <joaopedrouchoacampos@gmail.com>
2024-07-15 20:10:18 -0300

    add: Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import '../Header/Header.css'
-import logo from '../../assets/imgs/logo-header.svg'
+import React from "react";
+import "../Header/Header.css";
+import logo from "../../assets/imgs/logo-header.svg";
+import lupa from "../../assets/icons/lupa.svg";
+import cart from "../../assets/icons/mini-cart.svg";
 
 function Header() {
   return (
     <header>
-      <div className='menu-wrapper'>
-        <img src={logo} alt="" />
+      <div className="menu-wrapper">
+        <img className="logo" src={logo} alt="" />
         <div className="search-bar">
-          <input type="text" />
-          <img src="" alt="" />
+          <input type="text" placeholder="Pesquisar produto..." />
+          <img src={lupa} alt="" />
         </div>
-        <div className="button-container"></div>
+        <div className="button-container">
+          <a href="">Cadastre-se</a>
+          <button>Entrar</button>
+        </div>
+        <img className="cart" src={cart} alt="" />
       </div>
+        <ul className="list-wrapper">
+          <li>Home</li>
+          <li>Produtos</li>
+          <li>Categorias</li>
+          <li>Meus Pedidos</li>
+        </ul>
     </header>
   );
 }
 
-export default Header
+export default Header;
